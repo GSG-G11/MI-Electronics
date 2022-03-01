90969428ff388c45868c510a1169ae1b413ed38d
mo7amedehab97 <gazacityface@gmail.com>
2022-03-01 14:27:49 +0200

    remove the x.powerd by express header relates #5

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const app = express();
 app.set('port', process.env.PORT || 8080);
 
 app.use(compression());
+app.disable('x-powered-by')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
